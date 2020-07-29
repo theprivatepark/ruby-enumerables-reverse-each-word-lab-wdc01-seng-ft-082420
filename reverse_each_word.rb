4bad1477d7a60309d971a259d3188c29ffaaaa1d
Anney Park <anneypark@gmail.com>
2020-07-29 22:00:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,5 @@
 #{ "Hello there, and how are you?" }
 
-def reverse_each_word_w_each(sentence)
-  og_array = sentence.split("")
-  returned_array = []
-  og_array.each do |element|
-    returned_array << element.reverse
-  end 
-  returned_array.join("")
-end
-
-
 def reverse_each_word(sentence)
   array = sentence.split("")
   reversed = []
