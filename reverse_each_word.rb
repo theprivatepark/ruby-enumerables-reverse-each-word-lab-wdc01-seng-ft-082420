37532d08ecbdb9a85b903d470832c45932ab8ab8
Anney Park <anneypark@gmail.com>
2020-07-29 22:03:49 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+#{ "Hello there, and how are you?" }
+
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  reversed = []
+  array.collect do |element|
+    reversed << element.reverse
+  end
+  reversed.join(" ")
+end
